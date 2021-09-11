cb3dd85c8810febe81787623c81107464e9bbc8d
Joan Salvatella <joan@bookline.io>
2021-09-12 00:13:57 +0200

    spacings

--- speech.py
+++ speech.py
@@ -1,9 +1,6 @@
 import speech_recognition as sr
 import pyttsx3 
 
-
-
-
 def create_engine():
     """
     Create Speech engine 
@@ -11,27 +8,24 @@ def create_engine():
     engine = pyttsx3.init('sapi5')
     voices = engine.getProperty('voices')
     engine.setProperty('voice', voices[0].id)
-
-    return engine 
-
+    return engine
 
 def speak(engine, text):
-    #print(text)
+    print(text)
     engine.say(text)
     engine.runAndWait()
 
 def takeCommand():
-    r=sr.Recognizer()
+    r = sr.Recognizer()
     with sr.Microphone() as source:
-        #print("Listening...")
-        audio=r.listen(source)
+        print("Listening...")
+        audio = r.listen(source)
 
         try:
-            statement=r.recognize_google(audio,language='es-ES')
-            #print(f"TU: {statement}\n")
+            statement = r.recognize_google(audio, language='es-ES')
+            print(f"TU: {statement}\n")
 
         except Exception as e:
-            #speak("Disculpe, no lo entiendo")
             return "None"
         return statement
 
